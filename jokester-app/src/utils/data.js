f45c1f7bfaf89ca5403b47640f447cbe193a9ef2
Christian Karlsson <kodman91@gmail.com>
2023-05-06 22:43:52 +0200

    added a data util file and setup .env

---
+++ jokester-app/src/utils/data.js
@@ -0,0 +1,44 @@
+export const categories = [
+  {
+    name: 'Funny',
+  },
+  {
+    name: 'Memes',
+  },
+  {
+    name: 'Anime & Manga',
+  },
+  {
+    name: 'Animals',
+  },
+  {
+    name: 'Lifestyle',
+  },
+  {
+    name: 'Gaming',
+  },
+  {
+    name: 'Comic',
+  },
+  {
+    name: 'History',
+  },
+  {
+    name: 'Sports',
+  },
+  {
+    name: 'Tech',
+  },
+  {
+    name: 'Crypto',
+  },
+  {
+    name: 'Savage',
+  },
+  {
+    name: 'Wholesome',
+  },
+  {
+    name: 'Politics',
+  },
+]
