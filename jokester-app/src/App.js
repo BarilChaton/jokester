8dc7587060616547bb19d7f7e3c41922355f0edb
Christian Karlsson <kodman91@gmail.com>
2023-05-06 02:31:16 +0200

    Initial commit

---
+++ jokester-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen">
+      <div className='flex w-screen h-screen justify-center items-center'>
+        <h1 className="text-[5em] font-bold">HELLO WORLD!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
