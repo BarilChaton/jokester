7578634454f597ec726c1d1c4982a2f8b251b161
Christian Karlsson <kodman91@gmail.com>
2023-05-07 14:07:19 +0200

    added tween to loginButton

--- jokester-app/src/components/HeroBar/LoginButton.jsx
+++ jokester-app/src/components/HeroBar/LoginButton.jsx
@@ -10,19 +10,58 @@ const LoginButton = (props) => {
   const [ extend, setExtend ] = useState(false)
   const [ buttonText, setButtonText ] = useState()
 
-
-
   useLayoutEffect(() => {
-    if (loggedIn) {
-      setButtonText('UserName')
-    } else {
-      setButtonText('Sign Up/Login')
+    const handleHover = () => {
+      if(!loggedIn) {
+        extendButton().then(() => {
+          setButtonText('Sign in/Sign up')
+        })
+      } else {
+        extendButton().then(() => {
+          setButtonText(user.name)
+        })
+      }
     }
-  }, [ loggedIn ])
 
-  useLayoutEffect(() => {
+    const handleLeave = () => {
+      if(!loggedIn) {
+        returnButton().then(() => {
+          setButtonText('')
+        })
+      } else {
+        returnButton().then(() => {
+          setButtonText('')
+        })
+      }
+    }
 
-  }, [])
+    const signUpButton = document.querySelector(".sign-up-button")
+        signUpButton.addEventListener("mouseenter", handleHover)
+        signUpButton.addEventListener("mouseleave", handleLeave)
+
+        return () => {
+            signUpButton.removeEventListener("mouseenter", handleHover)
+            signUpButton.removeEventListener("mouseleave", handleLeave)
+        }
+  }, [loggedIn, user])
+
+  const extendButton = () => {
+    setExtend(true)
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve()
+      }, 300)
+    })
+  }
+
+  const returnButton = () => {
+      setExtend(false)
+      return new Promise((resolve) => {
+          setTimeout(() => {
+              resolve()
+          }, 300)
+      })
+    }
 
   function handleClick() {
     if (!loggedIn) {
@@ -32,8 +71,24 @@ const LoginButton = (props) => {
 
   return (
     <div className='w-[15vw] flex justify-end h-auto my-[2px] px-2 text-white font-bold text-lg'>
-      <button onClick={handleClick} className='flex flex-row h-[40px] w-[40px] items-center justify-center bg-blue-400 text-white rounded-full p-0 m-3'>
-        {!loggedIn ? <FaRegUser /> : <img src={user.image} alt="user" className='w-[30px] h-[30px] rounded-full' />}
+      <button onClick={handleClick} className={`
+        flex 
+        flex-row 
+        h-[40px] 
+        items-center 
+        justify-center 
+        bg-blue-400 
+        text-white 
+        rounded-full
+        sign-up-button
+        p-0 
+        m-3 
+        transition-width 
+        SignUpEasing 
+        duration-SignUpTransTime 
+        ${extend ? "w-SignUpExtend" : "w-SignUpNormal"}
+        `}>
+          {!loggedIn ? <FaRegUser /> : <img src={user.image} alt="user" className='w-[30px] h-[30px] rounded-full' />}
       </button>      
     </div>
   )
